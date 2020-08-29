37b7534685f75311c784fccca82b702fe5974773
Mark Harfouche <mark.harfouche@gmail.com>
2020-08-29 17:38:02 -0400

    update config.sub

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,9 @@
 set -x
 export CPPFLAGS="${CPP_FLAGS} -I$PREFIX/include "
-echo $HOST
-echo $BUILD
+
+# Get an updated config.sub and config.guess needed for aarch64
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+
 # disable server components
 ./configure \
     --prefix=$PREFIX  \
