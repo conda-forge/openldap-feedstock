aaf637ed013c41ee3e81604b3557fac8ee5a5a69
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2021-10-01 09:32:27 +0100

    Create bld.bat

---
+++ recipe/bld.bat
@@ -0,0 +1,15 @@
+# Get an updated config.sub and config.guess
+cp %BUILD_PREFIX%\share\gnuconfig\config.* build
+cp %BUILD_PREFIX%\share\gnuconfig\config.* contrib\ldapc++
+
+
+set ac_cv_func_memcmp_working=yes
+
+# disable server components
+./configure \
+    --prefix=%LIBRARY_PREFIX%  \
+    --disable-slapd \
+    --disable-slurpd \
+    --with-yielding_select=yes
+make -j${CPU_COUNT}
+make install
