5e2a9bda20441a58bf8b94f4b121abfa7c82c495
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2021-10-01 11:11:31 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,11 @@
 # Get an updated config.sub and config.guess
-cp %BUILD_PREFIX%\share\gnuconfig\config.* build
-cp %BUILD_PREFIX%\share\gnuconfig\config.* contrib\ldapc++
-
+copy %BUILD_PREFIX%\share\gnuconfig\config.* build
+copy %BUILD_PREFIX%\share\gnuconfig\config.* contrib\ldapc++
 
 set ac_cv_func_memcmp_working=yes
 
 # disable server components
-./configure \
-    --prefix=%LIBRARY_PREFIX%  \
-    --disable-slapd \
-    --disable-slurpd \
-    --with-yielding_select=yes
+./configure --prefix=%LIBRARY_PREFIX%  --disable-slapd --disable-slurpd --with-yielding_select=yes
+make depend
 make
 make install
